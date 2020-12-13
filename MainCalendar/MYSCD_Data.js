0f14d77a46f2309a19a20c8d4292abc94b884511
DEFAULTGROUP <41226022+defaultgroup@users.noreply.github.com>
2020-12-13 13:55:52 +0900

    메인 캘린더 업로드

---
+++ MainCalendar/MYSCD_Data.js
@@ -0,0 +1,13 @@
+var CalendarData={
+    "byUser":[
+        {"ID":"elroy","Calendar":[
+            {"Title":"형식언어 기말","Date":"2020-12-15","Time":"18:00-19:30","Content":"형식언어 시험"},
+            {"Title":"컴네 기말","Date":"2020-12-16","Time":"all","Content":"정보섬..."},
+            {"Title":"프로젝트 제출","Date":"2020-12-17","Time":"16:00-16:00","Content":"노트북"},
+        ]},
+        {"ID":"qwerty","Calendar":[
+            {"Title":"웹프밍 기말","Date":"2020-12-14","Time":"13:00-14:30","Content":"코딩테스트"},
+            {"Title":"컴네 기말","Date":"2020-12-16","Time":"all","Content":"정보섬..."}
+        ]}
+    ]
+};
